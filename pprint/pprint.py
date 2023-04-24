df311624244eb5ef5c35746f924614859841fe6b
Matthias Urlichs <matthias@urlichs.de>
2023-04-24 17:20:13 +0200

    pprint: clean up
    - proper indentation
    - simplify output if zero or one members
    - emit multi-line tuples/lists

--- pprint/pprint.py
+++ pprint/pprint.py
@@ -14,14 +14,36 @@ def _pprint(obj, stream=None, indent=1, width=80, depth=None):
         stream = sys.stdout
 
     if isinstance(obj, dict):
+        if not obj:
+            stream.write("{}")
+            return
+        if len(obj) == 1:
+            stream.write("{ ")
+            for k, v in obj.items():
+                _pprint(k, stream, indent+1, width, depth)
+                stream.write(": ")
+                _pprint(v, stream, indent+1, width, depth)
+            stream.write(" }")
+            return
         stream.write("{\n")
         for k, v in obj.items():
-            stream.write("  ")
-            _pprint(k, stream, indent, width, depth)
+            stream.write("  "*indent)
+            _pprint(k, stream, indent+1, width, depth)
             stream.write(": ")
-            _pprint(v, stream, indent, width, depth)
+            _pprint(v, stream, indent+1, width, depth)
             stream.write(",\n")
-        stream.write("}")
+        stream.write("  "*indent+"}")
+    elif isinstance(obj, (list,tuple)):
+        if not obj:
+            stream.write("[]" if isinstance(obj, list) else "()")
+            return
+        stream.write("[\n" if isinstance(obj, list) else "(\n")
+        for v in obj:
+            stream.write("  "*indent)
+            _pprint(v, stream, indent+1, width, depth)
+            stream.write(",\n")
+        stream.write("  "*indent+("]" if isinstance(obj, tuple) else ")"))
+
     else:
         print(repr(obj), file=stream, end="")
 
