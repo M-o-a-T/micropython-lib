3ad50602731145698fb495c05b6bf832e271e215
Matthias Urlichs <matthias@urlichs.de>
2023-05-17 14:39:54 +0200

    Blacked

--- pprint/pprint.py
+++ pprint/pprint.py
@@ -20,29 +20,29 @@ def _pprint(obj, stream=None, indent=1, width=80, depth=None):
         if len(obj) == 1:
             stream.write("{ ")
             for k, v in obj.items():
-                _pprint(k, stream, indent+1, width, depth)
+                _pprint(k, stream, indent + 1, width, depth)
                 stream.write(": ")
-                _pprint(v, stream, indent+1, width, depth)
+                _pprint(v, stream, indent + 1, width, depth)
             stream.write(" }")
             return
         stream.write("{\n")
         for k, v in obj.items():
-            stream.write("  "*indent)
-            _pprint(k, stream, indent+1, width, depth)
+            stream.write("  " * indent)
+            _pprint(k, stream, indent + 1, width, depth)
             stream.write(": ")
-            _pprint(v, stream, indent+1, width, depth)
+            _pprint(v, stream, indent + 1, width, depth)
             stream.write(",\n")
-        stream.write("  "*indent+"}")
-    elif isinstance(obj, (list,tuple)):
+        stream.write("  " * indent + "}")
+    elif isinstance(obj, (list, tuple)):
         if not obj:
             stream.write("[]" if isinstance(obj, list) else "()")
             return
         stream.write("[\n" if isinstance(obj, list) else "(\n")
         for v in obj:
-            stream.write("  "*indent)
-            _pprint(v, stream, indent+1, width, depth)
+            stream.write("  " * indent)
+            _pprint(v, stream, indent + 1, width, depth)
             stream.write(",\n")
-        stream.write("  "*indent+("]" if isinstance(obj, list) else ")"))
+        stream.write("  " * indent + ("]" if isinstance(obj, list) else ")"))
 
     else:
         print(repr(obj), file=stream, end="")
